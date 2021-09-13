35e6f42c6adc54d2a4cda6ef294f523c2979a6f2
Francis Emmanuel Galguerra <emmanuel.g@sunmoontechnologies.com>
2021-09-13 16:51:30 +0800

    new: added migration files and model for pets

---
+++ database/migrations/2021_09_13_084653_create_pet_details_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePetDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pet_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('pet_id');
+
+            $table->string('name')->unique();
+            $table->string('nickname')->nullable()->default('');
+            $table->enum('gender', ['Male', 'Female', ''])->nullable()->default('');
+            $table->date('birthday');
+            
+            $table->float('weight')->unsigned();
+            $table->float('height')->unsigned();
+            $table->string('color')->nullable()->default('');
+            $table->boolean('friendly')->default(false);
+
+            $table->timestamps();
+
+            $table->foreign('pet_id')->references('id')->on('pets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pet_details');
+    }
+}
